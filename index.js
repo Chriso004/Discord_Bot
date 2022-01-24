713a118b987cae274e8efd399c500e93777c12b4
Chriso004 <qudcksrlf1@gmail.com>
2022-01-24 14:13:45 +0900

    init bot

--- index.js
+++ index.js
@@ -3,5 +3,8 @@ const config = require('./config.json');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
 
-
 client.login(config.BOT_TOKEN);
+
+client.on("ready", () => {
+    console.log('Logged in  as ${client.user.tag}!');
+});
