89ff6ff1142e2a256a1e624bef4c1eb1706a8bfd
Chriso004 <qudcksrlf1@gmail.com>
2022-02-09 11:25:35 +0900

    번역 기능 추가

--- commands/translate.js
+++ commands/translate.js
@@ -1,16 +1,39 @@
+const translate = require("googletrans").default;
 const { SlashCommandBuilder } = require("@discordjs/builders");
 
 module.exports = {
-    data: SlashCommandBuilder()
+    data: new SlashCommandBuilder()
         .setName("번역")
         .setDescription("번역하고 싶은 말을 적어보세요!")
-        .setStringOption(option => {
+        .addStringOption(option => {
             return option
                 .setName("text")
                 .setDescription("단어나 문장을 입력하세요.")
                 .setRequired(true);
         }),
         async execute(interaction) {
-            
+            const text = interaction.options.getString("text");
+            const language = "ko"
+            const result = translateText(text, language);
+            result.then(async result => {
+                await interaction.reply(`${text} => ${result}`);
+                console.log(isKor(text));
+            })
         }
+}
+
+async function translateText(text, language = "ko") {
+    if(isKor(text)) language = "en";
+    try {
+        const result = await translate(text, language);
+        return result.text;
+    }
+    catch(error) {
+        console.log(error);
+    }
+}
+
+function isKor(str) {
+    const regExp = /[ㄱ-ㅎㅏ-ㅣ가-힣]/g;
+    return regExp.test(str) == true ? true : false;
 }
